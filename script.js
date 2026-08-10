4c5cc4f4689dee498f0c441d3e41d3ff3eee011c
CyberInsanityy <124412577+CyberInsanityy@users.noreply.github.com>
2026-08-10 18:36:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1263,8 +1263,8 @@ function renderDialog(product) {
       ...productFeatures.map(
         (feature) => `<li>${escapeHtml(feature)}</li>`
       ),
-      "<li>$5.99 handling fee applied to every order</li>
-<li>Please allow 7–10 business days for fulfillment</li>",
+     "<li>$5.99 handling fee applied to every order</li>",
+"<li>Please allow 7–10 business days for fulfillment</li>",
       "<li>Out-of-Box Warranty included</li>"
     ].join("");
   }
